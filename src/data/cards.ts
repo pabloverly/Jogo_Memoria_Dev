ac4d077df4770d079d3ab01fc5e97236efe06d63
pabloverly <pablopierre.pv@gmail.com>
2023-02-25 00:57:47 -0300

    Proejto Estudo

---
+++ src/data/cards.ts
@@ -0,0 +1,26 @@
+export const cards = [
+  {
+    id: '1',
+    back: 'https://miro.medium.com/max/1024/1*33CwBYkmnMfpA9Djup22Jw.png' 
+  },
+  {
+      id: '2',
+      back: 'https://www.pngitem.com/pimgs/m/520-5202823_nodejs-node-js-logo-png-transparent-png.png' 
+  },
+  {
+      id: '3',
+      back: 'https://sdtimes.com/wp-content/uploads/2019/08/typescriptfeature-490x305.png' 
+  },
+  {
+      id: '4',
+      back: 'https://www.docker.com/wp-content/uploads/2022/03/vertical-logo-monochromatic.png' 
+  },  
+  {
+      id: '5',
+      back: 'https://pngimg.com/uploads/github/small/github_PNG19.png' 
+  },  
+  {
+      id: '6',
+      back: 'https://3.bp.blogspot.com/-JZTpD8wVYC4/W72qUBBQTiI/AAAAAAAABbY/o3zUaTLoif47n3ptEeyn2LihV5e13vKuwCLcBGAs/s1600/visual-studio-code.png' 
+  }
+];
---
+++ src/readme.md
@@ -0,0 +1,23 @@
+<h1 align="center">
+    Jogo da Memória
+</h1>
+
+<p align="center">
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-features-e-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-licença">Licença</a>
+</p>
+
+###  💻 Projeto
+
+Jogo da Memória do Desenvolvedor.
+
+### 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com [React](https://pt-br.reactjs.org/).
+
+
+
+## 📝 Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
